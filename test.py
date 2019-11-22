837567f62f4510b8c76930e09320f55767faf4fa
conor-mel1 <conormelody123@yahoo.com>
2019-11-22 12:52:43 +0000

    Wrote a second simple program

---
+++ test.py
@@ -0,0 +1,11 @@
+'''
+Created on 21 Nov 2019
+
+@author: conor
+'''
+
+x = [1,2,3,4,5]
+
+for i in x:
+    print(i)
+    
---
+++ test2.py
@@ -0,0 +1,11 @@
+'''
+Created on 22 Nov 2019
+
+@author: conor
+'''
+
+y=[1,2,3,4,5]
+
+for i in y:
+    print(i)
+    
